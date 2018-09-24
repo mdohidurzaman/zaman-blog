5cf0a5a4c788831207af728c4775d7c6fa1c1e34
Md Ohidurzaman <zamanohidur@yahoo.com>
2018-09-24 03:42:47 +0000

    Add one to many assosiation with veiw debugger

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 10, maximum: 100 }
     validates :description, presence: true, length: { minimum: 15, maximum: 1000 }
-    
+    validates :user_id, presence: true
 end
